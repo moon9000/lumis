0b3e772891e92af466bce627af550646ce7ee585
moon9000 <sebros02@gmail.com>
2024-05-26 15:10:32 +0200

    add bo folder

---
+++ bo/config/plugins.js
@@ -0,0 +1,26 @@
+module.exports = {
+    //
+    graphql: {
+      config: {
+        endpoint: '/graphql',
+        shadowCRUD: true,
+        playgroundAlways: false,
+        depthLimit: 7,
+        amountLimit: 100,
+        apolloServer: {
+          tracing: false,
+        },
+      },
+    },
+    "vercel-deploy": {
+      enabled: true,
+      config: {
+        deployHook:
+          "https://api.vercel.com/v1/integrations/deploy/prj_<deploy-hook>",
+        apiToken: process.env.VERCEL_DEPLOY_PLUGIN_API_TOKEN,
+        appFilter: "your-app-name-on-vercel",
+        teamFilter: "your-team-id-on-vercel",
+        roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+      },
+    },
+  };
